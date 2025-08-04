09c395380a7c49d048914ce9dd1ddb56d7d1fa98
Philipp Kleber <philipp.t.kleber@gmail.com>
2025-08-05 00:14:45 +0200

    make sure buildx is used and add some logs

--- oakestra-vm-ubuntu/build.sh
+++ oakestra-vm-ubuntu/build.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 ARCH=$1
 VERSION="${2:-"25.04"}"
 
+echo "Importing ubuntu cloud image..."
 curl \
     --proto '=https' \
     --tlsv1.2 \
@@ -15,5 +16,8 @@ curl \
     "https://cloud-images.ubuntu.com/releases/${VERSION}/release/ubuntu-${VERSION}-server-cloudimg-${ARCH}-root.tar.xz" \
   | xzcat \
   | docker import --platform "linux/${ARCH}" - "axiphi/oakestra-vm-ubuntu-raw:${VERSION}"
+echo "Imported ubuntu cloud image"
 
-docker build --load --platform "linux/${ARCH}" --tag "axiphi/oakestra-vm-ubuntu:${VERSION}" --build-arg "VERSION=${VERSION}" .
+echo "Building ubuntu VM base image..."
+docker buildx build --load --platform "linux/${ARCH}" --tag "axiphi/oakestra-vm-ubuntu:${VERSION}" --build-arg "VERSION=${VERSION}" .
+echo "Built ubuntu VM base image"
